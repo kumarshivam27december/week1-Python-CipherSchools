96ac5b23a6bc5b748cf0878f7120d4ec59aae672
shivamkumar <kumarshivam16062004@gmail.com>
2022-11-25 15:22:08 +0530

    version 4.0

---
+++ class2-basic_python-datatype_variable_comments.py
@@ -0,0 +1,40 @@
+
+# we can use '#' to comment in a single line
+
+
+
+'''for multiline 
+line comment
+we use triple quoto'''
+
+
+
+# this is a comment
+
+
+
+'''this is 
+also
+a comment'''
+
+
+a='a'
+print(type(a))
+b=255
+print(id(b))
+
+print(id(255))
+
+
+c='aaaa'
+print(id(c))
+
+print(id('aaaa'))
+
+d=74683272899327279382783
+print(d+1)
+
+
+
+
+
