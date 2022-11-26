cb947f4029f1e063d4b9f7da5e946ec6f51ef515
shivamkumar <kumarshivam16062004@gmail.com>
2022-11-26 22:42:47 +0530

    version 5.0

---
+++ class5-operators_and_expressions-CipherSchools.py
@@ -0,0 +1,69 @@
+print(10+10)
+print(10-5)
+print(10/3)
+print(isinstance(10.0,int))
+print(10%3)
+print(10//3)
+print(9//3)
+print(8//3)
+print(2//3)
+print(4//3)
+print("jatin"+"katyal")
+print("abc"*6)
+print("jatin" " " "katyal")
+print(1==2)
+print(1!=2)
+print(1<2)
+print(2>3)
+print(2<=3)
+# lexicographic order a will come then b then g
+
+# whenever we are tryuing to compare the string
+print("a"<"A")
+print(True or False)
+print(True and False)
+print(True and 1)
+print(1 and 0)
+print(1 and 5)
+print(0 and 1)
+print(5 and 1)
+print(5 and 2)
+print(2 and 5)
+
+# how python read boolean
+# python does not have any true boolean 
+
+print(isinstance(True,int))
+print(type(True))
+print(bool(0))
+print(bool(1))
+print(bool(2))
+print(bool(3))
+print(bool(10000000000000000000000000000000000000))
+
+# a or b = b (if  b  is truthy)
+# a or b = a (if b is falsy)
+# a and  b = a (if b is truthy)
+# a and  b = b (if b is falsy)
+
+# a or true=true
+# a or false=a
+# a and true=true
+# a and false=false
+print("shivam" and 6)
+print(6 and "shivam")
+print('' and 6)
+print(6 and '')
+print(1.6 or '')
+print(1.6 and '')
+print('' or 1.6)
+print(bool(''))
+print(bool([1,2,3,4,5,6,7,8,9,0]))
+print("" and 0)
+print(112 and '')
+
+
+
+
+
+
