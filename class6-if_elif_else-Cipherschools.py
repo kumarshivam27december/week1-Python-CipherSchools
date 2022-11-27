29cc6ccdbf9020fc9d811ac6301ba97a16e784cb
shivamkumar <kumarshivam16062004@gmail.com>
2022-11-27 20:24:47 +0530

    version 6.0

---
+++ class6-if_elif_else-Cipherschools.py
@@ -0,0 +1,63 @@
+a=True
+if a:
+    print("the value is true")
+print('end')
+
+
+
+b=False
+if b:
+    print("the value is true")
+else:
+    print('the value is false')
+print('end')
+
+c=5
+if c==3:
+    print("the value of c is 3")
+elif c==5:
+    print('the value of c is 5')
+else:
+    print('the value of c is neither 3 nor 5')
+print('end')
+
+
+
+
+q= 'can profile A access A access B '
+a='isfriend'
+b='isblocked'
+c='isAdmin'
+d='isMarkZuckerberg'
+
+
+if a and not b:
+    print("hi my name i ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
